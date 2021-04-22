b09e08df4e89e256c08653eafd6299908538fcc2
fuwei <454575238@qq.com>
2021-04-22 15:39:56 +0800

    feat(fork-form): add get & set for utils

--- src/components/fork-form/utils/common.ts
+++ src/components/fork-form/utils/common.ts
@@ -22,4 +22,34 @@ export function set<Entity = any, Output = any, Value = any>(
   ) {
     return entity
   }
+
+  return internalSet(entity, paths, value, removeIfUndefined)
+}
+
+function internalSet<Entity = any, Output = Entity, Value = any>(
+  entity: Entity,
+  paths: (string | number)[],
+  value: Value,
+  removeIfUndefined: boolean,
+): Output {
+  if (!paths.length) return (value as unknown) as Output
+
+  const [path, ...restPath] = paths
+  let clone: Record<typeof path, any>
+
+  if (!entity && typeof path === 'number') {
+    clone = []
+  } else if (Array.isArray(entity)) {
+    clone = [...entity]
+  } else {
+    clone = { ...entity }
+  }
+
+  if (removeIfUndefined && value === undefined && restPath.length === 1) {
+    delete clone[path][restPath[0]]
+  } else {
+    clone[path] = internalSet(clone[path], restPath, value, removeIfUndefined)
+  }
+
+  return clone as Output
 }
